b3c610099778a6eff48a5dcce56b2bcfba820774
Sofi Delgado <sofi5_09@hotmail.com>
2023-03-14 20:05:33 -0300

    Ejercicio 1 arrays

---
+++ js/Ejercicio-1.js
@@ -0,0 +1,7 @@
+const meses = ["Enero", "Febrero", "Marzo","Abril","Mayo", "Junio", "Julio", "Agosto", "Septiembre","Octubre","Noviembre","Diciembre"];
+
+document.write("<ul>");
+for(let i=0; i<meses.length;i++)
+{
+    document.write(`<li>${meses[i]}</li>`);
+}
