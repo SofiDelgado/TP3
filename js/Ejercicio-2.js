d62864f3eb47b53394d48922a48a7bdc45df4fcc
Sofi Delgado <sofi5_09@hotmail.com>
2023-03-14 21:08:16 -0300

    Se agregan los puntos que faltaban del ejercicio 2

--- js/Ejercicio-2.js
+++ js/Ejercicio-2.js
@@ -1,12 +1,35 @@
+function arreglo(ciudades) {
+    document.write(`<h3>Listado de ciudades</h3>`);
+
+    document.write("<ul>");
+    for(let i=0; i<ciudades.length;i++)
+    {
+        document.write(`<li>${ciudades[i]}</li>`);
+    }
+    document.write("</ul>");
+}
+
 let ciudades=[];
 
 do{
-ciudades.unshift(prompt(`Ingrese ciudades: `));
+ciudades.push(prompt(`Ingrese ciudades: `));
 }while(confirm("Desea seguir agregando datos?") ==true);
 
-document.write("<ul>");
-for(let i=0; i<ciudades.length;i++)
-{
-    document.write(`<li>${ciudades[i]}</li>`);
-}
-document.write("</ul>")
+arreglo(ciudades);
+
+document.write(`<h2>Longitud del arreglo: ${ciudades.length}</h2>`);
+document.write(`<h4>Primera posicion: ${ciudades[0]}`);
+document.write(`<h4>Tercera posicion: ${ciudades[2]}`);
+document.write(`<h4>Ultima posicion: ${ciudades[ciudades.length - 1]}`);
+
+document.write(`<h2>Agregar Paris como ultima posicion</h2>`);
+ciudades.push(`Paris`);
+
+arreglo(ciudades);
+
+alert("El elemento "+ciudades[1] + " ocupa la segunda posicion");
+
+document.write(`<h2>Sustituir la segunda posicion con "Barcelona"</h2>`);
+ciudades[1] = "Barcelona";
+
+arreglo(ciudades);
